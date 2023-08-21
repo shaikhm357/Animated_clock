7df6175fd17547c877588adbe2c941dd40010109
Shaikh Mahboob <mahboob.shaikh@finclusionsolutions.com>
2023-08-21 21:55:51 +0530

    added color customization and save as img

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const faceColor = document.getElementById('face-color')
+const borderColor = document.getElementById('border-color')
+const lineColor = document.getElementById('line-color')
+const largeHandColor = document.getElementById('large-hand-color')
+const secondColor = document.getElementById('second-color')
+
 function clock() {
   const now = new Date()
   const canvas = document.getElementById('canvas')
@@ -15,11 +21,13 @@ function clock() {
   ctx.lineWidth = 5
   ctx.lineCap = 'round'
 
-  //Draw clock face
+  //Draw clock face/border
   ctx.save()
   ctx.beginPath()
   ctx.lineWidth = 14
   ctx.strokeStyle = '#800000'
+  ctx.strokeStyle = borderColor.value
+  ctx.fillStyle = faceColor.value
   ctx.arc(0, 0, 142, 0, Math.PI * 2, true)
   ctx.stroke()
   ctx.fill()
@@ -27,6 +35,7 @@ function clock() {
 
   //draw hr marks lines
   ctx.save()
+  ctx.strokeStyle = lineColor.value
   for (let i = 0; i < 12; i++) {
     ctx.beginPath()
     ctx.rotate(Math.PI / 6)
@@ -39,6 +48,7 @@ function clock() {
   //Draw minuts lines
   ctx.save()
   ctx.lineWidth = 4
+  ctx.strokeStyle = lineColor.value
   for (let i = 0; i < 60; i++) {
     if (i % 5 !== 0) {
       ctx.beginPath()
@@ -55,13 +65,14 @@ function clock() {
   const min = now.getMinutes()
   const sec = now.getSeconds()
 
-  console.log(`${hr}:${min}:${sec}`)
+  //   console.log(`${hr}:${min}:${sec}`)
 
   //draw hr hand
   ctx.save()
 
-//   ctx.rotate((Math.PI / 6) * hr + (Math.PI / 360) * min + (Math.PI / 21600) * sec)
+  ctx.rotate((Math.PI / 6) * hr + (Math.PI / 360) * min + (Math.PI / 21600) * sec)
   ctx.strokeStyle = '#800000'
+  ctx.strokeStyle = largeHandColor.value
   ctx.lineWidth = 14
   ctx.beginPath()
   ctx.moveTo(-20, 0)
@@ -73,6 +84,7 @@ function clock() {
   ctx.save()
   ctx.rotate((Math.PI / 30) * min + (Math.PI / 1800) * sec)
   ctx.strokeStyle = '#800000'
+  ctx.strokeStyle = largeHandColor.value
   ctx.lineWidth = 8
   ctx.beginPath()
   ctx.moveTo(-28, 0)
@@ -84,7 +96,9 @@ function clock() {
   ctx.save()
   ctx.rotate((Math.PI / 30) * sec)
   ctx.strokeStyle = '#FF7F50'
+  ctx.strokeStyle = secondColor.value
   ctx.fillStyle = '#FF7F50'
+  ctx.fillStyle = secondColor.value
   ctx.lineWidth = 6
   ctx.beginPath()
   ctx.moveTo(-30, 0)
@@ -100,3 +114,14 @@ function clock() {
 }
 requestAnimationFrame(clock)
 // clock()
+
+const saveAsImage = document.getElementById('save-as-img')
+saveAsImage.addEventListener('click', () => {
+  //   e.preventDefault()
+  const dataUrl = canvas.toDataURL('image/png')
+  const link = document.createElement('a')
+  link.download = 'clock.png'
+  link.href = dataUrl
+  link.click()
+})
+
